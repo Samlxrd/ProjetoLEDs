bd25b213b2cb8817eab50723d48e1e3380bec9c6
Samlxrd <samlord.dzn@gmail.com>
2025-01-31 20:43:36 -0300

    Implementando interrupção nos botões A e B + controle dos números do display via Botões

--- ProjetoLEDs.c
+++ ProjetoLEDs.c
@@ -4,29 +4,22 @@
 #include "hardware/clocks.h"
 #include "pico/bootrom.h"
 #include "ws2812.pio.h"
+#include "numeros.h"
 
 // Definição de constantes
 #define A_BUTTON 5
 #define B_BUTTON 6
 #define LED_RED 13
 #define WS2812_PIN 7
-#define WS2812_LED_COUNT 25
 #define JOYSTICK_BUTTON 22
 
 // Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
-uint8_t red_value = 30; // Intensidade do vermelho
+uint8_t red_value = 20; // Intensidade do vermelho
 uint8_t green_value = 0; // Intensidade do verde
-uint8_t blue_value = 0; // Intensidade do azul
+uint8_t blue_value = 5; // Intensidade do azul
 
 uint32_t last_time;
-
-bool numero_0[WS2812_LED_COUNT] = {
-    0, 1, 1, 1, 0,
-    0, 1, 0, 1, 0, 
-    0, 1, 0, 1, 0, 
-    0, 1, 0, 1, 0, 
-    0, 1, 1, 1, 0
-};
+uint8_t current_frame = 0;
 
 // Protótipo das Funções
 void setup(void);
@@ -47,15 +40,16 @@ int main()
     // Habilitando interrupção da gpio no botão do joystick,
     // Auxiliando o desenvolvimento e testes
     gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, 1, & gpio_irq_handler);
+    gpio_set_irq_enabled(A_BUTTON, GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(B_BUTTON, GPIO_IRQ_EDGE_FALL, true);
+
+    set_led(numeros[0], red_value, green_value, blue_value); // Inicializa o programa colocando o número 0 no display.
 
     while(true) {
-        if(!gpio_get(A_BUTTON)) {
-            gpio_put(LED_RED, 1);
-        } else {
-            gpio_put(LED_RED, 0);
-        }
-        
-        set_led(numero_0, red_value, green_value, blue_value);
+        // LED Vermelho piscando alternando estado a cada 2s. (Apenas teste)
+        gpio_put(LED_RED, 1);
+        sleep_ms(2000);
+        gpio_put(LED_RED, 0);
         sleep_ms(2000);
     }
 
@@ -116,11 +110,23 @@ void set_led(bool *frame, uint8_t r, uint8_t g, uint8_t b)
 static void gpio_irq_handler(uint gpio, uint32_t events){
 
     uint32_t current_time = to_us_since_boot(get_absolute_time());
-    if (current_time - last_time > 40000) // Delay de 40ms para debounce
+    if (current_time - last_time > 100000) // Delay de 100ms para debounce
     {
         last_time = current_time;
 
         switch (gpio) {
+        case 5:
+            if (current_frame != 9) {   // Se o frame atual for 9, não é possível aumentar o número do contador no led
+                current_frame++;        // Incrementa o índice do contador de frames e carrega ele no display com as cores predefinidas.
+                set_led(numeros[current_frame], red_value, green_value, blue_value); 
+            } 
+            break;
+        case 6:
+            if (current_frame) {        // Se o frame atual for 0, não é possível diminuir o número do contador no led
+                current_frame--;        // Decrementa o índice do contador de frames e carrega ele no display com as cores predefinidas.
+                set_led(numeros[current_frame], red_value, green_value, blue_value);
+            } 
+            break;
 
         case 22:
             printf("HABILITANDO O MODO GRAVAÇÃO");
